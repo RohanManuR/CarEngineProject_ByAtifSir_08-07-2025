201a0da7fae2bd54ee01d329fd91b683f7cb8ab2
rohan <rohanrmanu99@gmail.com>
2025-07-08 15:15:47 +0530

    [UC1]ADDED:Created and Retrieved a Simple Car Bean

---
+++ src/main/java/com/blab/entity/Car.java
@@ -0,0 +1,9 @@
+package com.blab.entity;
+
+public class Car {
+
+    public void start()
+    {
+        System.out.println("car started,....");
+    }
+}
--- src/test/java/com/blab/entity/AppTest.java
+++ src/test/java/com/blab/entity/AppTest.java
@@ -1,4 +1,4 @@
-package com.blab.main;
+package com.blab.entity;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
